22ea016eced098d9f3dba426b6ab40da28d23d49
Aref Rezaei <arefrezaei.dev@gmail.com>
2024-01-20 13:42:01 +0330

    query side

--- src/Presentation/Bank.Api/Controllers/CustomersController.cs
+++ src/Presentation/Bank.Api/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using Bank.Api.DTOs;
+using Bank.Api.Queries;
 using Bank.Domain.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -30,26 +31,15 @@ namespace Bank.Api.Controllers
             await _mediator.Send(command, cancellationToken);
             return Ok();
         }
-        [HttpGet, Route("{id:guid}", Name = "GetCustomer")]
-        public async Task<IActionResult> GetCustomer(Guid id, CancellationToken cancellationToken = default)
-        {
-            //var query = new CustomerById(id);
-            //var result = await _mediator.Send(query, cancellationToken);
-            //if (null == result)
-            //    return NotFound();
-            //return Ok(results);
-            return Ok();
-        }
 
         [HttpGet]
         public async Task<IActionResult> Get(CancellationToken cancellationToken = default)
         {
-            //var query = new CustomersArchive();
-            //var results = await _mediator.Send(query, cancellationToken);
-            //if (null == results)
-            //    return NotFound();
-            //return Ok(results);
-            return Ok();
+            var query = new CustomersArchive();
+            var results = await _mediator.Send(query, cancellationToken);
+            if (null == results)
+                return NotFound();
+            return Ok(results);
         }
         #endregion
     }
---
+++ src/Presentation/Bank.Api/Queries/CustomersArchive.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Bank.Api.Queries
+{
+    public record CustomerArchiveItem
+    {
+        public CustomerArchiveItem(Guid id, string firstname, string lastname)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+        public Guid Id { get; }
+        public string Firstname { get; }
+        public string Lastname { get; }
+    }
+    public class CustomersArchive : IRequest<IEnumerable<CustomerArchiveItem>> { }
+}
