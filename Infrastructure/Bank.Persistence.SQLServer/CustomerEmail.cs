7e6ea9a1f6866891d0d5e5883cb306c4b62f116c
Aref Rezaei <arefrezaei.dev@gmail.com>
2024-01-20 13:50:13 +0330

    customer sql persistence was complete

--- Infrastructure/Bank.Persistence.SQLServer/CustomerEmail.cs
+++ Infrastructure/Bank.Persistence.SQLServer/CustomerEmail.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bank.Persistence.SQLServer.DBContext
+namespace Bank.Persistence.SQLServer
 {
-    internal class CustomerDbContext
-    {
-    }
+    public record CustomerEmail(Guid CustomerId, string Email);
 }
