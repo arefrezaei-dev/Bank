5754906214d8591fccb92ae81a9274d23025833a
Aref Rezaei <arefrezaei.dev@gmail.com>
2024-01-20 16:05:38 +0330

    sqlserver persistence for eventsourcing was init

---
+++ Infrastructure/Bank.Persistence.SQLServer.EventSourcing/SQLAggregateRepository.cs
@@ -0,0 +1,34 @@
+﻿using Bank.Domain;
+using Bank.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Persistence.SQLServer.EventSourcing
+{
+    public class SQLAggregateRepository<TA, TKey> : IAggregateRepository<TA, TKey>
+        where TA : class, IAggregateRoot<TKey>
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Public Methods
+        public async Task PersistAsync(TA aggregateRoot, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RehydrateAsync(TKey key, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ src/Core/Bank.Domain/IAggregateRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bank.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Domain
+{
+    public interface IAggregateRepository<TA,TKey>
+        where TA:class,IAggregateRoot<TKey>
+    {
+        Task PersistAsync(TA aggregateRoot, CancellationToken cancellationToken = default);
+        Task RehydrateAsync(TKey key, CancellationToken cancellationToken = default);
+    }
+}
